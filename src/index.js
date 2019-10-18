2c9e8820e7225c6e32ec834385a4c56ee90ac807
Lars Marius Bjørneseth <lars.bjorneseth@headit.no>
2019-10-18 13:18:04 +0200

    fix: error in type def

--- src/index.js
+++ src/index.js
@@ -2,33 +2,33 @@ const express = require('express');
 const expressGQL = require('express-graphql');
 const { buildSchema } = require('graphql');
 
-let superHeroes = require('../var/data/marvel-wikia-data');
+let superHeroes = require('../var/data/marvel-wikia-data.json');
 
 // The GraphQL schema which should be attached to the specific endpoint
 let schema = buildSchema(`
     type Query {
         superHeroByHairColor(color: String!): [SuperHero]
     },
-    type SuperHero: {
-        page_id: Int,
-        name: String,
-        urlslug: String,
-        ID: String,
-        ALIGN: String,
-        EYE: String,
-        HAIR: String,
-        SEX: String,
-        GSM: String,
-        ALIVE: String,
-        APPEARANCES: Int,
-        FIRST APPEARANCE: String,
+    type SuperHero {
+        page_id: Int
+        name: String
+        urlslug: String
+        ID: String
+        ALIGN: String
+        EYE: String
+        HAIR: String
+        SEX: String
+        GSM: String
+        ALIVE: String
+        APPEARANCES: Int
+        FIRSTAPPEARANCE: String
         Year: Int
     }
 `);
 
 // The root resolver object (contains the mapping of actions to functions)
 let rootResolverObj = {
-  getSuperHeroByHairColor: (color) => superHeroes.map((hero) => hero.HAIR === color)
+  // getSuperHeroByHairColor: (color) => superHeroes.map((hero) => hero.HAIR === color)
 };
 
 const app = express();
